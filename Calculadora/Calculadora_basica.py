99b0339ea03f06c3de5c00183cfea669b7465136
Alejandro Rios <alejandro.rios.d.angel@gmail.com>
2022-01-25 16:59:34 -0600

    Inicio
    Inicio de mi calculaodra pero no inicia el programa

---
+++ Calculadora/Calculadora_basica.py
@@ -0,0 +1,18 @@
+#Calculadora basica que haga suma,resta,multiplicacion y division
+
+
+
+def main() :
+    
+    n = input('Ingresa los numeroes entre espacios: \n')
+    print("\t")
+
+
+    print("Estos son los numeros ingresados: ")
+
+    respuesta = input("\n Desea continuar? Y/N: ")
+    if respuesta == "Y":
+        print("Funciono")
+        print(n)
+
+            
